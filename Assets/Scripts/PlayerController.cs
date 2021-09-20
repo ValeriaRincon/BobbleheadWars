40b3e5951e1bf3c41c09e28b39c17ea05b357026
Valeria Rincon <v.rincon558@gmail.com>
2021-09-20 09:43:18 -0400

    Ch. 4 - Added joints and head physics

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 50.0f;
+    public Rigidbody head;
     private CharacterController characterController;
 
     // Start is called before the first frame update
@@ -18,5 +19,18 @@ public class PlayerController : MonoBehaviour
     {
         Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         characterController.SimpleMove(moveDirection * moveSpeed);
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        if (moveDirection == Vector3.zero)
+        {
+            //TODO
+        }
+        else
+        {
+            head.AddForce(transform.right * 150, ForceMode.Acceleration);
+        }
     }
 }
