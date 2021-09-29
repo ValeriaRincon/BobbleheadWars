ea1d6d8d80e7933766bff84f1e809b386f6ae578
Valeria Rincon <v.rincon558@gmail.com>
2021-09-29 10:52:57 -0400

    Ch. 7 - Added shooting and Alien death sounds

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -6,21 +6,20 @@ public class Gun : MonoBehaviour
 {
     public GameObject bulletPrefab;
     public Transform launchPosition;
+    private AudioSource audioSource;
 
     void fireBullet()
     {
-        //1
         GameObject bullet = Instantiate(bulletPrefab) as GameObject;
-        //2
         bullet.transform.position = launchPosition.position;
-        //3
         bullet.GetComponent<Rigidbody>().velocity = transform.parent.forward * 100;
+        audioSource.PlayOneShot(SoundManager.Instance.gunFire);
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
