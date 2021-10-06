bfbf9193b3ad9173457a314b2659dfd58aac716c
Valeria Rincon <v.rincon558@gmail.com>
2021-10-06 11:32:07 -0400

    Ch. 8 - Fixed script order and Added Alien decapitation

--- Assets/Scripts/Alien.cs
+++ Assets/Scripts/Alien.cs
@@ -9,6 +9,9 @@ public class Alien : MonoBehaviour
     public Transform target;
     public float navigationUpdate;
     public UnityEvent OnDestroy;
+    public Rigidbody head;
+    public bool isAlive = true;
+
     private float navigationTime = 0;
     private NavMeshAgent agent;
 
@@ -21,26 +24,43 @@ public class Alien : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (target != null)
+        if (isAlive)
         {
-            navigationTime += Time.deltaTime;
-            if(navigationTime > navigationUpdate)
+            if (target != null)
             {
-                agent.destination = target.position;
-                navigationTime = 0;
+                navigationTime += Time.deltaTime;
+                if (navigationTime > navigationUpdate)
+                {
+                    agent.destination = target.position;
+                    navigationTime = 0;
+                }
             }
         }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        Die();
-        SoundManager.Instance.PlayOneShot(SoundManager.Instance.alienDeath);
+        if (isAlive)
+        {
+            Die();
+            SoundManager.Instance.PlayOneShot(SoundManager.Instance.alienDeath);
+        }
     }
 
     public void Die()
     {
+        isAlive = false;
+        head.GetComponent<Animator>().enabled = false;
+        head.isKinematic = false;
+        head.useGravity = true;
+        head.GetComponent<SphereCollider>().enabled = true;
+        head.gameObject.transform.parent = null;
+        head.velocity = new Vector3(0, 26.0f, 3.0f);
+
         OnDestroy.Invoke();
+        OnDestroy.RemoveAllListeners();
+        SoundManager.Instance.PlayOneShot(SoundManager.Instance.alienDeath);
+        head.GetComponent<SelfDestruct>().Initiate();
         Destroy(gameObject);
     }
 }
